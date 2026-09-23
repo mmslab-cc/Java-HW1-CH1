e5515da34aa62d54cea73cb34a42e7caff5b88c3
b812113014-collab <b812113014@tmu.edu.tw>
2026-09-23 14:35:03 +0800

    Create HW1_P26.java

---
+++ HW1_P26.java
@@ -0,0 +1,14 @@
+public class HW1_P26 {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        System.out.println("變數num原本的值是：" + num);
+
+        num = 20;
+
+        System.out.println("重新指定num的值");
+        System.out.println("變數num更新後的值是：" + num);
+    }
+}
