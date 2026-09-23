71d0813dc6982dbaffcc13b3dfaa819df23bde51
b812113014-collab <b812113014@tmu.edu.tw>
2026-09-23 14:35:25 +0800

    Create HW1_P27.java

---
+++ HW1_P27.java
@@ -0,0 +1,16 @@
+public class HW1_P27 {
+
+    public static void main(String[] args) {
+
+        int num1, num2;
+
+        num1 = 7;
+
+        System.out.println("變數num1的值是：" + num1);
+
+        num2 = num1;
+
+        System.out.println("將num1的值指定給num2");
+        System.out.println("變數num2的值是：" + num2);
+    }
+}
