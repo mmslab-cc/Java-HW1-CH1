5bdef16627dfe35156a6cfc08ef7f7b72f7599f7
b812113014-collab <b812113014@tmu.edu.tw>
2026-09-23 14:35:52 +0800

    Create HW1_P31.java

---
+++ HW1_P31.java
@@ -0,0 +1,21 @@
+public class HW1_P31 {
+
+    public static void main(String[] args) {
+
+        System.out.println("2 + 3 等於：" + (2 + 3));
+        System.out.println("4 * 5 等於：" + (4 * 5));
+
+        int num1 = 6;
+        int num2 = 4;
+
+        int sum = num1 + num2;
+
+        System.out.println("變數num1的值是：" + num1);
+        System.out.println("變數num2的值是：" + num2);
+        System.out.println("num1 + num2 等於：" + sum);
+
+        num1 = num1 + 1;
+
+        System.out.println("num1加1之後是：" + num1);
+    }
+}
