201f932e714e0cb08c2f9015d1264ec09f209687
b812113014-collab <b812113014@tmu.edu.tw>
2026-09-23 14:33:58 +0800

    Create HW1_P16.java

---
+++ HW1_P16.java
@@ -0,0 +1,10 @@
+public class HW1_P16 {
+
+    public static void main(String[] args) {
+
+        System.out.println('B');
+        System.out.println("我正在學習Java!");
+        System.out.println(456);
+        System.out.println(3.14);
+    }
+}
