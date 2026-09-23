6610b422a56919ca31a208d269acd7bd03c3feef
b812113014-collab <b812113014@tmu.edu.tw>
2026-09-23 14:34:36 +0800

    Create HW1_P18.java

---
+++ HW1_P18.java
@@ -0,0 +1,11 @@
+public class HW1_P18 {
+
+    public static void main(String[] args) {
+
+        System.out.println("顯示反斜線：\\");
+        System.out.println("顯示單引號：\'");
+
+        System.out.println("八進位101的字元是：\101");
+        System.out.println("十六進位0061的字元是：\u0061");
+    }
+}
