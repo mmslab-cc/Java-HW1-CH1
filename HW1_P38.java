cb70028e9b7f8cc2305d5d6726472a7200ce3b35
b812113014-collab <b812113014@tmu.edu.tw>
2026-09-23 14:36:28 +0800

    Create HW1_P38.java

---
+++ HW1_P38.java
@@ -0,0 +1,13 @@
+public class HW1_P38 {
+
+    public static void main(String[] args) {
+
+        double height = 175.8;
+
+        System.out.println("原本的身高是：" + height + "公分");
+
+        int newHeight = (int) height;
+
+        System.out.println("轉成int型態後：" + newHeight + "公分");
+    }
+}
